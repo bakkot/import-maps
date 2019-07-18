ad90ab6108a7e806518fd3506b3f4423bdb2559c
Michael Ficarra <mficarra@shapesecurity.com>
2019-07-18 14:49:47 -0700

    more progress

--- reference-implementation/__tests__/composition.js
+++ reference-implementation/__tests__/composition.js
@@ -17,24 +17,25 @@ function composeMaps(mapLikes) {
     let newMap = parseFromString(JSON.stringify(mapLike), mapBaseURL);
     map = appendMap(map, newMap);
   }
-  return map;
+  return JSON.parse(JSON.stringify(map));
 }
 
 describe('Composition', () => {
   it('should compose with the empty map on the left', () => {
-    let map = {
-      imports: { 'https://a': 'https://b' },
+    let map = parseFromString(JSON.stringify({
+      imports: { 'https://a/': ['https://b/'] },
       scopes: {
-        'https://c': { 'https://d': 'https://e' },
+        'https://c/': { 'https://d/': ['https://e/'] },
       },
-    };
+    }), mapBaseURL);
 
-    let resultMap = appendMap(
-      parseFromString("{}", mapBaseURL),
-      parseFromString(JSON.stringify(map), mapBaseURL),
-    );
+    let resultMap = appendMap(parseFromString("{}", mapBaseURL), map);
+
     expect(resultMap).toStrictEqual(map);
     expect(resultMap.imports).not.toBe(map.imports);
+    Object.entries(resultMap.imports).forEach(([k, v]) => {
+      expect(v).not.toBe(map.imports[k]);
+    });
     expect(resultMap.scopes).not.toBe(map.scopes);
     Object.entries(resultMap.scopes).forEach(([k, v]) => {
       expect(v).not.toBe(map.scopes[k]);
@@ -42,19 +43,20 @@ describe('Composition', () => {
   });
 
   it('should compose with the empty map on the right', () => {
-    let map = {
-      imports: { 'https://a': 'https://b' },
+    let map = parseFromString(JSON.stringify({
+      imports: { 'https://a/': ['https://b/'] },
       scopes: {
-        'https://c': { 'https://d': 'https://e' },
+        'https://c/': { 'https://d/': ['https://e/'] },
       },
-    };
+    }), mapBaseURL);
 
-    let resultMap = appendMap(
-      parseFromString(JSON.stringify(map), mapBaseURL),
-      parseFromString("{}", mapBaseURL),
-    );
+    let resultMap = appendMap(map, parseFromString("{}", mapBaseURL));
+
     expect(resultMap).toStrictEqual(map);
     expect(resultMap.imports).not.toBe(map.imports);
+    Object.entries(resultMap.imports).forEach(([k, v]) => {
+      expect(v).not.toBe(map.imports[k]);
+    });
     expect(resultMap.scopes).not.toBe(map.scopes);
     Object.entries(resultMap.scopes).forEach(([k, v]) => {
       expect(v).not.toBe(map.scopes[k]);
@@ -64,19 +66,19 @@ describe('Composition', () => {
   it('should compose maps that do not interact in any way', () => {
     expect(composeMaps([
       {
-        imports: { 'https://a': 'https://b' },
+        imports: { 'https://a/': 'https://b/' },
       },
       {
-        imports: { 'https://c': 'https://d' },
+        imports: { 'https://c/': 'https://d/' },
       },
       {
-        imports: { 'https://e': 'https://f' },
+        imports: { 'https://e/': 'https://f/' },
       },
     ])).toStrictEqual({
       imports: {
-        'https://a': 'https://b',
-        'https://c': 'https://d',
-        'https://e': 'https://f',
+        'https://a/': ['https://b/'],
+        'https://c/': ['https://d/'],
+        'https://e/': ['https://f/'],
       },
       scopes: {},
     });
@@ -85,19 +87,44 @@ describe('Composition', () => {
   it('should compose maps that interact via cascading', () => {
     expect(composeMaps([
       {
-        imports: { 'https://c': 'https://d' },
+        imports: { 'https://c/': 'https://d/' },
       },
       {
-        imports: { 'https://b': 'https://c' },
+        imports: { 'https://b/': 'https://c/' },
       },
       {
-        imports: { 'https://a': 'https://b' },
+        imports: { 'https://a/': 'https://b/' },
       },
     ])).toStrictEqual({
       imports: {
-        'https://a': 'https://d',
-        'https://b': 'https://d',
-        'https://c': 'https://d',
+        'https://a/': ['https://d/'],
+        'https://b/': ['https://d/'],
+        'https://c/': ['https://d/'],
+      },
+      scopes: {},
+    });
+  });
+
+  it('should compose maps with fallbacks that interact via cascading', () => {
+    expect(composeMaps([
+      {
+        imports: { 'https://e/': ['https://g/', 'https://h/'] },
+      },
+      {
+        imports: {
+          'https://c/': ['https://f/'],
+          'https://b/': ['https://d/', 'https://e/'],
+        },
+      },
+      {
+        imports: { 'https://a/': ['https://b/', 'https://c/'] },
+      },
+    ])).toStrictEqual({
+      imports: {
+        'https://a/': ['https://d/', 'https://g/', 'https://h/', 'https://f/'],
+        'https://b/': ['https://d/', 'https://g/', 'https://h/'],
+        'https://c/': ['https://f/'],
+        'https://e/': ['https://g/', 'https://h/'],
       },
       scopes: {},
     });
@@ -106,29 +133,29 @@ describe('Composition', () => {
   it('should compose maps that are using the virtualisation patterns we expect to see in the wild', () => {
     expect(composeMaps([
       {
-        imports: { 'std:built-in': 'https://built-in-enhancement-1' },
+        imports: { 'std:built-in': 'https://built-in-enhancement-1/' },
         scopes: {
-          'https://built-in-enhancement-1': { 'std:built-in': 'std:built-in' },
+          'https://built-in-enhancement-1/': { 'std:built-in': 'std:built-in' },
         },
       },
       {
-        imports: { 'std:built-in': 'https://built-in-enhancement-2' },
+        imports: { 'std:built-in': 'https://built-in-enhancement-2/' },
         scopes: {
-          'https://built-in-enhancement-2': { 'std:built-in': 'std:built-in' },
+          'https://built-in-enhancement-2/': { 'std:built-in': 'std:built-in' },
         },
       },
       {
-        imports: { 'std:built-in': 'https://built-in-enhancement-3' },
+        imports: { 'std:built-in': 'https://built-in-enhancement-3/' },
         scopes: {
-          'https://built-in-enhancement-3': { 'std:built-in': 'std:built-in' },
+          'https://built-in-enhancement-3/': { 'std:built-in': 'std:built-in' },
         },
       },
     ])).toStrictEqual({
-      imports: { 'std:built-in': 'https://built-in-enhancement-3' },
+      imports: { 'std:built-in': ['https://built-in-enhancement-3/'] },
       scopes: {
-        'https://built-in-enhancement-1': { 'std:built-in': 'std:built-in' },
-        'https://built-in-enhancement-2': { 'std:built-in': 'https://built-in-enhancement-1' },
-        'https://built-in-enhancement-3': { 'std:built-in': 'https://built-in-enhancement-2' },
+        'https://built-in-enhancement-1/': { 'std:built-in': ['std:built-in'] },
+        'https://built-in-enhancement-2/': { 'std:built-in': ['https://built-in-enhancement-1/'] },
+        'https://built-in-enhancement-3/': { 'std:built-in': ['https://built-in-enhancement-2/'] },
       },
     });
   });
@@ -136,34 +163,34 @@ describe('Composition', () => {
   it('should compose "nested" scopes', () => {
     expect(composeMaps([
       {
-        imports: { 'https://a': 'https://b' },
+        imports: { 'https://a/': 'https://b/' },
         scopes: {
-          '/x/y': { 'https://c': 'https://d' },
-          '/x/y/z': { 'https://e': 'https://f' },
+          'https://example.com/x/y': { 'https://c/': 'https://d/' },
+          'https://example.com/x/y/z': { 'https://e/': 'https://f/' },
         },
       },
       {
-        imports: { 'https://m': 'https://n' },
+        imports: { 'https://m/': 'https://n/' },
         scopes: {
-          '/x/y/z': {
-            'https://g': 'https://a',
-            'https://h': 'https://c',
-            'https://i': 'https://e',
+          'https://example.com/x/y/z': {
+            'https://g/': 'https://a/',
+            'https://h/': 'https://c/',
+            'https://i/': 'https://e/',
           },
         },
       },
     ])).toStrictEqual({
       imports: {
-        'https://a': 'https://b',
-        'https://m': 'https://n',
+        'https://a/': 'https://b/',
+        'https://m/': 'https://n/',
       },
       scopes: {
-        '/x/y': { 'https://c': 'https://d' },
-        '/x/y/z': {
-          'https://e': 'https://f',
-          'https://g': 'https://b',
-          'https://h': 'https://d',
-          'https://i': 'https://f',
+        'https://example.com/x/y': { 'https://c/': 'https://d/' },
+        'https://example.com/x/y/z': {
+          'https://e/': 'https://f/',
+          'https://g/': 'https://b/',
+          'https://h/': 'https://d/',
+          'https://i/': 'https://f/',
         },
       },
     });
--- reference-implementation/lib/composer.js
+++ reference-implementation/lib/composer.js
@@ -2,9 +2,26 @@
 
 exports.appendMap = (baseMap, newMap) => {
   let resultMap = {
-    imports: Object.assign({}, baseMap.imports),
+    imports: Object.fromEntries([
+      ...Object.entries(baseMap.imports).map(([moduleSpecifier, fallbacks]) =>
+        [moduleSpecifier, [...fallbacks]]
+      ),
+      ...Object.entries(newMap.imports).map(([moduleSpecifier, fallbacks]) =>
+        [moduleSpecifier, fallbacks.flatMap(fallback => applyCascadeWithContexts(fallback, [baseMap.imports]))]
+      ),
+    ]),
     scopes: Object.assign({}, baseMap.scopes),
   };
   return resultMap;
 };
+
+
+// string -> Array<Map<string, Array<string>>> -> Array<string>
+function applyCascadeWithContexts(moduleSpecifier, applicableMapContexts) {
+  if (applicableMapContexts.length < 1) {
+    return [moduleSpecifier];
+  }
+  let [head, ...tail] = applicableMapContexts;
+  return moduleSpecifier in head ? head[moduleSpecifier] : applyCascadeWithContexts(moduleSpecifier, tail);
+}
 
